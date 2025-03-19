4dfeee97a897b3b947b4b7a6ed8b66e7873c8c92
Shero Shvan <sheroshvan@outlook.dk>
2025-03-19 06:19:44 +0100

    firstRabbitMQ

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// run RabbitMQ server
+// docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:4.0-management 
+// Create a connection to the RabbitMQ server
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+// new connection
+var conn = await factory.CreateConnectionAsync();
+
+// new channel
+var channel = await conn.CreateChannelAsync();
+
+// Declare a queue
+await channel.QueueDeclareAsync(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+// call back
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray(); // Convert the message to a byte array (decode)
+    var message = Encoding.UTF8.GetString(body); // Decode the message
+    Console.WriteLine($"Received message: {message}"); // Display the message
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Waiting for messages...");
+
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+
+// Create a connection to the RabbitMQ server
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+//new connection
+
+  var conn = await factory.CreateConnectionAsync();
+
+//new channel
+
+var channel = await conn.CreateChannelAsync();
+
+// Declare a queue
+await channel.QueueDeclareAsync(
+    queue: "letterbox",
+    durable: false,
+     exclusive: false,
+     autoDelete: false,
+     arguments: null
+);
+
+// Create a message
+
+string message = "This is my first Message!";
+// Convert the message to a byte array (encode)
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+// publish the message to the queue
+await channel.BasicPublishAsync(
+    exchange: "",
+    routingKey: "letterbox",
+    mandatory: false,
+    basicProperties: new BasicProperties(), // Specify the type argument explicitly
+    body: encodedMessage
+);
+
+
+Console.WriteLine($"Published message: {message}");
