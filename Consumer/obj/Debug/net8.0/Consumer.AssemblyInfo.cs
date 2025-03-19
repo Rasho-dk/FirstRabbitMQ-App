227c3bf3e412f73c23840160385c3c0308712be8
Shero Shvan <sheroshvan@outlook.dk>
2025-03-19 06:21:05 +0100

    .

--- Consumer/obj/Debug/net8.0/Consumer.AssemblyInfo.cs
+++ Consumer/obj/Debug/net8.0/Consumer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfeee97a897b3b947b4b7a6ed8b66e7873c8c92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
